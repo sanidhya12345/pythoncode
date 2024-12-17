0de5cc15e0fce4e7ed592f4f844ab97df0eb601e
sanidhya12345 <sanidhya.varshney_cs19@gla.ac.in>
2024-12-17 09:45:26 +0530

    paypaloa

---
+++ dp/paypaloa.py
@@ -0,0 +1,11 @@
+s=input()
+k=int(input())
+dp=[0 for i in range(len(s))]
+dp[0]=1
+maxLen=0
+for i in range(1,len(s)):
+    if abs(ord(s[i-1])-ord(s[i]))<=k:
+        dp[i]=dp[i-1]+1
+    else:
+        dp[i]=1
+print(max(dp))
