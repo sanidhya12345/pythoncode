375266afae0cdbd1e5cfbdac99fd29a94f5f5dbd
sanidhya12345 <sanidhya.varshney_cs19@gla.ac.in>
2026-06-30 17:00:11 +0530

    zip trick

---
+++ PythonTricksCP/ziptricks.py
@@ -0,0 +1,14 @@
+# declaring the list of elements
+
+a=["hello","i","am","sanidhya","varshney"]
+b=[1,2,3,4,5]
+for a,b in zip(a,b):
+    print(a,b)
+
+'''
+hello 1
+i 2
+am 3
+sanidhya 4
+varshney 5
+'''
